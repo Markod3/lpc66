123eb778cc0595468b9389467dc9b3c8a73bb94c
Marcod3 <marco.hernandezirc@uanl.edu.mx>
2022-09-08 22:54:41 -0500

    agregamos descripcion del commit

--- Pokemon2.py
+++ Pokemon2.py
@@ -2,6 +2,11 @@ import requests
 #
 # Nombre: Marco Antonio Hernandez Iracheta
 # Matricula: 1568448
+# Script en python que consulta el api de pokemon
+# para listar los nombres de pokemon pero se le agrego
+# interaccion para que listaras mas pokemon segun se vaya requiriendo
+# Contribuyo: Marco Hernandez
+# 08/09/2022
 def get_pokemon (url='http://pokeapi.co/api/v2/pokemon-form/', offset=0):
     args = {'offset':offset} if offset else{}
 
